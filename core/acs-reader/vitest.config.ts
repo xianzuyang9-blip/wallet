85c949a2c2b842699d1f11d1fb940cfd65e6d682
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-06-01 14:37:48 +0200

    fix(core-acs-reader): undo vitest config modification
    Signed-off-by: Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>

--- core/acs-reader/vitest.config.ts
+++ core/acs-reader/vitest.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
                     browser: {
                         enabled: true,
                         provider: playwright({
+                            trace: 'off',
                             screenshot: 'off',
                             video: 'off',
                         }),
